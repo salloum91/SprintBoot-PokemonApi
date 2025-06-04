1c5d9a2257ebfa2cf0d8717b28be2ba1ffa36082
salloum91 <aboward70@gmail.com>
2025-06-03 17:33:39 -0700

    Remove api folder

--- pokemon_api/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import Pokemon
-from .serializers import PokemonSerializer
-from rest_framework.response import Response
-from rest_framework import status
-
-class PokemonViewSet(viewsets.ModelViewSet):
-    queryset = Pokemon.objects.all()
-    serializer_class = PokemonSerializer
